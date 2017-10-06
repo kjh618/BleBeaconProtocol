076aa238d46ba46160f2e77e8b39f875d4a687fe
kjh <kimjihoon6188@daum.net>
2017-10-06 22:18:30 +0900

    Separate DataItemAdapter and remove DataItemView, Change Scan Result to show raw data

---
+++ app/src/main/java/kr/hs/gshs/blebeaconprotocol/DataItemAdapter.java
@@ -0,0 +1,66 @@
+package kr.hs.gshs.blebeaconprotocol;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kjh on 2017-10-06.
+ */
+
+public class DataItemAdapter extends BaseAdapter{
+    ArrayList<DataItem> items;
+    Context mContext;
+    LayoutInflater mInflater;
+
+    DataItemAdapter(Context context, LayoutInflater inflater) {
+        super();
+        mContext = context;
+        mInflater = inflater;
+        items = new ArrayList<>();
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    public void addItem(DataItem item) {
+        items.add(item);
+    }
+
+    public void removeItem(int position) {
+        items.remove(position);
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        view = mInflater.inflate(R.layout.data_item, null);
+
+        TextView textViewType = (TextView) view.findViewById(R.id.textView_type);
+        TextView textViewLength = (TextView) view.findViewById(R.id.textView_length);
+        TextView textViewData = (TextView) view.findViewById(R.id.textView_data);
+
+        DataItem item = items.get(position);
+        textViewType.setText(item.getType());
+        textViewData.setText(item.getData());
+        textViewLength.setText(String.valueOf(item.getLength()));
+
+        return view;
+    }
+}
